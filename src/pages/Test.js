b43a5a85589ad5f9ea84a2a43e6374f44ec138ee
vinny <vincentmarcos@hotmail.com>
2017-08-15 15:24:45 +1000

    styling added

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import CircularProgress from 'material-ui/CircularProgress'
 import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedIn: false,
-      loading: false
+      ssid: null,
+      password: null
     }
   }
+  // Perform the BlinkUp (flashing)
+  startDisconnectBinkUp = () => {
+    window.BU.startDisconnectFlash({},
+    function () {
+      // On complete
+      // Hide the canvas and progress bar now that BlinkUp is complete
+      // hideFlashingElements();
+      // hidePollingProgress();
 
-  startBinkUp = () => {
+      // Display the results as success
+      // setInstruction('Complete');
+      // document.getElementById('status').style.display = 'block';
+      // showResult('Your device should now be flashing amber', true);
+      return;
+    }
+    )
+  }
+
+  startBinkUp = (ssid,password) => {
+    console.log('credentials',ssid,password)
     let blinkup = window.BU // or blinkup = BU; if not using modules
     blinkup.getConfigId("dd8f9c0b4af5d0417bdfdf7916754d22", null, "production", function(err, configId){
         if (err){
             // Handle error
         } else {
             var networkConfig = new blinkup.NetworkConfig({
-                ssid: "someSSID",
-                password: "somePassword"
+                ssid: ssid,
+                password: password
             });
 
             blinkup.startNetworkFlash(configId, networkConfig,
@@ -40,43 +58,54 @@ class HomePage extends Component {
         }
     });
   }
+  handleWifiChange = (event,newString) => {
+    console.log(newString)
+    this.setState({ ssid: newString})
+  }
+  handlePasswordChange = (event,newString) => {
+    console.log(newString)
+    this.setState({ password: newString})
+  }
 
   render() {
     return (
       <div>
-          <div id="form">
-            <section className="submission-inputs">
-              <button onClick={this.startBinkUp}>Connect Production Device</button>
-              <button className="white-button" onclick="blinkUp('disconnect')">Clear Device Configuration</button>
-            </section> /* end submission-inputs */
-
-          </div> /* end form */
-
+          <MuiThemeProvider>
+            <TextField
+              onChange={this.handleWifiChange}
+              floatingLabelText='Wifi name'/>
+          </MuiThemeProvider>
+          <br/>
+          <MuiThemeProvider>
+            <TextField
+              onChange={this.handlePasswordChange}
+              floatingLabelText='Wifi password'
+              type='Password'/>
+          </MuiThemeProvider>
+          <br/>
+          <MuiThemeProvider>
+            <RaisedButton
+              style={{width: '40%',marginRight: 'auto',marginLeft: 'auto'}}
+              onTouchTap={() => this.startBinkUp(this.state.ssid,this.state.password)}
+              label='Connect C1'
+            />
+          </MuiThemeProvider> <br/>
+          <MuiThemeProvider>
+            <RaisedButton
+              style={{width: '40%',marginRight: 'auto',marginLeft: 'auto'}}
+              onTouchTap={this.startDisconnectBinkUp}
+              label='Clear C1 Configuration'/>
+            </MuiThemeProvider>
           <div className="lightbox" id="lightbox-blinkUp">
-            <div>
-              <p id="lightbox-close">&#10005;</p>
-              /* Instructions on current step */
-              <h3 id="current-instruction">Align device sensor to the screen</h3>
-            </div>
-
-            <div id="status">
-              <center>
-                <img id="progress-img" src="https://electricimp.com/public/img/progress.gif"/>
-                <p id="result"></p>
-              </center>
-            </div>
 
             <div id="BU-progress">
             </div>
 
-            /* For countdown */
             <div id="countdown" className="canvas">
               <p></p>
             </div>
-
-            /* For Flashing */
             <div style={{backgroundColor: 'white'}}>
-              <canvas id="BU-canvas" className="canvas"></canvas>
+              <canvas style={{borderSize: '2px',borderStyle: 'solid'}} id="BU-canvas" className="canvas"></canvas>
             </div>
           </div>
       </div>
