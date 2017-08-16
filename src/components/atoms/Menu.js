571b830ff6c48dda6fb1c0c2985adc70c35601be
Vincent Lloyd <vincent.lloyd@protonmail.com>
2017-08-16 14:30:44 +1000

    Create material-ui menu component

---
+++ src/components/atoms/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import WimoThemeProvider from '../../styles/WimoThemeProvider'
+import Menu from 'material-ui/Menu' 
+
+export default function SimpleMenu (props){
+  let mutableProps = { ...props }
+  let children = mutableProps.children
+  delete mutableProps.children
+  return(
+    <WimoThemeProvider>
+      <Menu { ...mutableProps }>
+        {children}
+      </Menu>
+    </WimoThemeProvider>
+  )
+}
