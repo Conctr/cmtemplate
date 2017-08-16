994150f5ba3986de511358f47a57e25d3a2a6e4a
charles smith <charlesrichmondsmith@gmail.com>
2017-08-16 15:17:56 +1000

    more sass changes

---
+++ src/components/atoms/Avatar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Avatar from 'material-ui/Avatar';
+import FileFolder from 'material-ui/svg-icons/file/folder';
+import FontIcon from 'material-ui/FontIcon';
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+
+import {
+  blue300,
+  indigo900,
+  orange200,
+  deepOrange300,
+  pink400,
+  purple500,
+} from 'material-ui/styles/colors';
+
+const style = {margin: 5};
+
+/**
+ * Examples of `Avatar` using an image, [Font Icon](/#/components/font-icon), [SVG Icon](/#/components/svg-icon)
+ * and "Letter" (string), with and without custom colors at the default size (`40dp`) and an alternate size (`30dp`).
+ */
+const UserAvatar = () => (
+  <List>
+    <ListItem
+      disabled={true}
+      leftAvatar={
+        <Avatar src="imgs/battery-icon.png" />
+      }
+    >
+      Image Avatar
+    </ListItem>
+  </List>
+);
+
+export default UserAvatar;
