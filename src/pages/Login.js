c4d8f8f5b0fceac0cfb5604e22c31f006e78d02c
Vincent Lloyd <vincent.lloyd@protonmail.com>
2017-08-16 16:44:51 +1000

    Edits to add home screen background image

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import bg from '../imgs/wimo-bg-y.png'
-import logo from '../imgs/wimo-logo-3.svg'
+import Background from '../imgs/wimo-bg-y.png'
+import Logo from '../imgs/wimo-logo-3.svg'
 import TextField from '../components/atoms/TextField'
 import RaisedButton from '../components/atoms/RaisedButton'
 import LoginModal from '../components/molecules/LoginModal'
@@ -8,6 +8,11 @@ import LoginModal from '../components/molecules/LoginModal'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import CircularProgress from 'material-ui/CircularProgress'
 import * as oauthApi from '../api/oAuth'
+
+// let splashStyle = {
+//   width: '100%',
+//   backgroundImage: 'url(' + Background + ')'
+// }
 
 class LoginPage extends Component {
   constructor(props) {
@@ -29,6 +34,7 @@ class LoginPage extends Component {
   }
 
   onInputChange = (e, newValue) => {
+    //handle user input and set password and email values
     this.setState({ 
       [e.target.id]: newValue 
     }) 
@@ -51,66 +57,70 @@ class LoginPage extends Component {
 
   render() {
     return (
-      <div className='welcome-container'>
-        {this.state.loading ? (
-          <MuiThemeProvider>
-            <CircularProgress/>
-          </MuiThemeProvider>
-        ) : (
-          <div>
-            <img src={ bg } alt='wine bottles' className='home-bg' />
-            <div className='welcome-dialogue'>
-              <img
-                src={ logo } alt='wimo logo' className='hero-logo' />
-              <div>
-                <div className='login-text-fields'>
-                  <TextField
-                    id={ 'email' }
-                    fullWidth={ true }
-                    onChange={ this.onInputChange }
-                    onEnterKeyDown={
+      <div
+        className='home-splash tip-top'
+        // style={ splashStyle }
+      >
+        <div className='welcome-container'>
+          {this.state.loading ? (
+            <MuiThemeProvider>
+              <CircularProgress/>
+            </MuiThemeProvider>
+          ) : (
+              <div className='welcome-dialogue'>
+                <img
+                  src={ Logo } alt='wimo logo' className='hero-logo' />
+                <div>
+                  <div className='login-text-fields'>
+                    <TextField
+                      id={ 'email' }
+                      fullWidth={ true }
+                      onChange={ this.onInputChange }
+                      onEnterKeyDown={
+                        () => this.submitToAuth(this.props.onSignIn)
+                      }
+                      value={ this.state.email }
+                      hintText='Email'
+                    />
+                    <TextField
+                      id={ 'password' }
+                      fullWidth={ true }
+                      onChange={ this.onInputChange }
+                      onEnterKeyDown={
+                        () => this.submitToAuth(this.props.onSignIn)
+                      }
+                      value={ this.state.password }
+                      hintText='Password'
+                      type='password'
+                    />
+                  </div>
+                  <RaisedButton
+                    className="login-page-button"
+                    label="Log in"
+                    onTouchTap={
                       () => this.submitToAuth(this.props.onSignIn)
                     }
-                    value={ this.state.email }
-                    hintText='Email'
                   />
-                  <TextField
-                    id={ 'password' }
-                    fullWidth={ true }
-                    onChange={ this.onInputChange }
-                    onEnterKeyDown={
-                      () => this.submitToAuth(this.props.onSignIn)
+                  <RaisedButton
+                    className="login-page-button"
+                    label="Log in with Google"
+                    onTouchTap={
+                      () => oauthApi.signIn('signin')
                     }
-                    value={ this.state.password }
-                    hintText='Password'
-                    type='password'
                   />
-                </div>
-                <RaisedButton
-                  className="login-page-button"
-                  label="Log in"
-                  onTouchTap={
-                    () => this.submitToAuth(this.props.onSignIn)
-                  }
-                />
-                <RaisedButton
-                  className="login-page-button"
-                  label="Log in with Google"
-                  onTouchTap={() => oauthApi.signIn('signin')}
-                />
-                <RaisedButton
-                  className ="login-page-button"
-                  label="Register"
-                  fullWidth={ true }
-                />
-                <LoginModal className="login-modal" />
-                </div>
+                  <RaisedButton
+                    className ="login-page-button"
+                    label="Register"
+                    fullWidth={ true }
+                  />
+                  <LoginModal className="login-modal" />
+              </div>
+              <div className='conctr-footer'>
+                <span style={{ color: 'white' }}>Powered by Conctr</span>
+              </div>
             </div>
-            <div className='conctr-footer'>
-              <span style={{color: 'white'}}>Powered by Conctr</span>
-            </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     )
   }
