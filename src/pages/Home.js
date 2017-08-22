2fff73e43b8886282e6d20228c43616c9efa1d16
Vincent Lloyd <vincent.lloyd@protonmail.com>
2017-08-22 11:13:32 +1000

    Clean home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import MuiThemeProvider from '../styles/WimoThemeProvider';
-import CircularProgress from 'material-ui/CircularProgress';
-import {Tabs, Tab} from 'material-ui/Tabs'
+import React, { Component } from 'react'
+import CircularProgress from 'material-ui/CircularProgress'
+import { Tabs, Tab } from 'material-ui/Tabs'
 import ClusterIcon from 'react-icons/lib/ti/flow-children'
 import DevicesIcon from 'react-icons/lib/go/circuit-board'
-import NewClusterModal from '../components/organisms/NewClusterModal';
-import DevicesPaper from '../components/organisms/DevicesPaper';
-import ClustersGrid from '../components/organisms/ClustersGrid';
+import NewClusterModal from '../components/organisms/NewClusterModal'
+import DevicesPaper from '../components/organisms/DevicesPaper'
+import ClustersGrid from '../components/organisms/ClustersGrid'
 
 class HomePage extends Component {
   constructor(props){
@@ -20,7 +19,7 @@ class HomePage extends Component {
   }
   handleChange = (value) => {
     this.setState({
-      value: value,
+      value: value
     });
   };
 
@@ -33,6 +32,7 @@ class HomePage extends Component {
       return cluster.id !== clusterId
     })})
   }
+
   render() {
     return (
       <div>
@@ -42,20 +42,27 @@ class HomePage extends Component {
             <p>Please select devices or clusters to continue</p>
           </div>
         ) : this.state.value === 'devices' ? (
-              <DevicesPaper handleError={this.props.handleError}/>
+          <DevicesPaper handleError={this.props.handleError}/>
         ) : this.state.value === 'clusters' ? (
           <div>
             <h1>Clusters</h1>
             {this.state.rules && JSON.stringify(this.state.rules)}
             {this.state.clusters ? (
               <div>
-                <NewClusterModal deviceIds={this.state.devicesData.data.map(device => (device.device_id))} addCluster={this.handleAddCluster}/>
+                <NewClusterModal
+                  deviceIds={
+                    this.state.devicesData.data.map(device => (
+                      device.device_id
+                    ))
+                  }
+                  addCluster={this.handleAddCluster}
+                />
                 {this.state.clusters.length > 0 ? (
                   <ClustersGrid
                     addCluster={this.handleAddCluster}
                     deleteCluster={this.handleDeleteCluster}
                     clusters={this.state.clusters}
-                    />
+                  />
                 ) : (
                   <h1>No clusters saved</h1>
                 )}
@@ -65,7 +72,7 @@ class HomePage extends Component {
             )}
           </div>
         ) : (
-          <h1>I dunno</h1>
+          false
         )}
       </div>
     )
