0f94ba8cb88f8c349e734c7b8306e23f1d7fe34d
Vincent Lloyd <vincent.lloyd@protonmail.com>
2017-08-23 16:50:14 +1000

    Small edits

--- src/components/molecules/ChipContainer.js
+++ src/components/molecules/ChipContainer.js
@@ -1,23 +1,24 @@
 import React from 'react'
-import Chip from '../atoms/Chip'
+import Chip from 'material-ui/Chip'
 
 
 export default function CustomTable(props) {
   return (<div className='chip-container'>
-    {props.sortedGraphs.map(graph =>(
+    { props.sortedGraphs.map(graph =>(
       <div key={graph.key}>
-      {graph.display ? (
+      { graph.display ? (
         <Chip
-        className='display-true'
-        style={{backgroundColor: '#ff9999',color: 'white'}}
-        onRequestDelete={() => props.handleGraphDelete(graph.key)}>
-          {graph.displayTitle}
+          className='display-true'
+          style={{ backgroundColor: '#ff9999',color: 'white' }}
+          onRequestDelete={ () => props.handleGraphDelete(graph.key) }
+        >
+          { graph.displayTitle }
         </Chip>
       ) : (
         <Chip
         className='display-false'
-        onTouchTap={() => props.handleGraphAdd(graph.key)}>
-          {graph.displayTitle}
+        onTouchTap={ () => props.handleGraphAdd(graph.key) }>
+          { graph.displayTitle }
         </Chip>
       )}
       </div>
