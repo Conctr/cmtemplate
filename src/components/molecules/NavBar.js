ecad0ea395181d18c8f72af2b66df066eea3acba
Mizuki Zenta <mizukisb@gmail.com>
2018-02-07 15:08:04 +1100

    navbar logo customisable

--- src/components/molecules/NavBar.js
+++ src/components/molecules/NavBar.js
@@ -1,106 +1,92 @@
-import React from 'react'
-import Avatar from 'material-ui/Avatar'
-import IconMenu from 'material-ui/IconMenu'
-import IconButton from 'material-ui/IconButton'
-import MenuItem from 'material-ui/MenuItem'
-import RaisedButton from 'material-ui/RaisedButton'
-import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more'
-import {
-  Toolbar,
-  ToolbarGroup,
-  ToolbarSeparator } from 'material-ui/Toolbar'
-import logo from '../../imgs/wimo-logo-y.svg'
-import {getUserDetails} from '../../api/oAuth'
-
+import React from "react";
+import Avatar from "material-ui/Avatar";
+import IconMenu from "material-ui/IconMenu";
+import IconButton from "material-ui/IconButton";
+import MenuItem from "material-ui/MenuItem";
+import RaisedButton from "material-ui/RaisedButton";
+import NavigationExpandMoreIcon from "material-ui/svg-icons/navigation/expand-more";
+import { Toolbar, ToolbarGroup, ToolbarSeparator } from "material-ui/Toolbar";
+import logo from "../../imgs/logoImage.jpg";
+import { getUserDetails } from "../../api/oAuth";
 
 export default class NavBar extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       value: 3,
       openMenu: false
-    }
+    };
   }
 
-  handleChange = (event, index, value) => this.setState({ value })
+  handleChange = (event, index, value) => this.setState({ value });
 
-  handleOnRequestChange = (value) => {
+  handleOnRequestChange = value => {
     this.setState({
       openMenu: value
-    })
-  }
+    });
+  };
 
   handleOpenMenu = () => this.setState({ openMenu: true });
 
   render() {
-    let firstName, lastName, avatarUser
-      getUserDetails()["firstname"]
-      ? firstName = getUserDetails()["firstname"]
-      : firstName = 'Set'
+    const logoImage = process.env.REACT_APP_LOGO_IMAGE_URL || logo;
+    let firstName, lastName, avatarUser;
+    getUserDetails()["firstname"]
+      ? (firstName = getUserDetails()["firstname"])
+      : (firstName = "Set");
 
+    getUserDetails()["lastname"]
+      ? (lastName = getUserDetails()["lastname"])
+      : (lastName = "Profile");
 
-      getUserDetails()["lastname"]
-      ? lastName = getUserDetails()["lastname"]
-      : lastName = 'Profile'
-
-      if(getUserDetails()["avatar"]){
-        avatarUser = getUserDetails()["avatar"]
-      } else {
-        avatarUser = 'https://mysticpants.com/_include/img/CONCTR-LOGO-MUSTARD-LINE.png'
-      }
+    if (getUserDetails()["avatar"]) {
+      avatarUser = getUserDetails()["avatar"];
+    } else {
+      avatarUser =
+        "https://mysticpants.com/_include/img/CONCTR-LOGO-MUSTARD-LINE.png";
+    }
 
     return (
       <div>
         {this.props.signedIn ? (
           <Toolbar>
-            <ToolbarGroup firstChild={ true }>
-              <img
-                className='navbar-logo'
-                src={ logo }
-                alt='wimo logo'
-              />
+            <ToolbarGroup firstChild={true}>
+              <img className="navbar-logo" src={logoImage} alt="app logo" />
             </ToolbarGroup>
             <ToolbarGroup>
               <ToolbarSeparator />
               <RaisedButton
-                primary={ true }
-                onTouchTap={ this.handleOpenMenu }
+                primary={true}
+                onTouchTap={this.handleOpenMenu}
                 icon={
                   <Avatar
                     // src="http://i.telegraph.co.uk/multimedia/archive/03388/enfield_3388479b.jpg"
                     src={avatarUser}
-                    size={ 30 }
+                    size={30}
                   />
                 }
-                label={ firstName + ' ' + lastName }
+                label={firstName + " " + lastName}
               />
               <IconMenu
-                className='float-menu'
+                className="float-menu"
                 iconButtonElement={
-                  <IconButton touch={ true }>
+                  <IconButton touch={true}>
                     <NavigationExpandMoreIcon />
                   </IconButton>
                 }
-                onRequestChange={ this.handleOnRequestChange }
-                open={ this.state.openMenu }
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+                onRequestChange={this.handleOnRequestChange}
+                open={this.state.openMenu}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                targetOrigin={{ horizontal: "right", vertical: "top" }}
               >
-                <a
-                  href='https://conctr.com'
-                  className='naked-href'
-                >
+                <a href="https://conctr.com" className="naked-href">
                   <MenuItem primaryText="About Conctr" />
                 </a>
-                <a
-                  href="mailto:support@conctr.com"
-                  className='naked-href'
-                >
+                <a href="mailto:support@conctr.com" className="naked-href">
                   <MenuItem primaryText="Email Conctr Support" />
                 </a>
                 <MenuItem
-                  onTouchTap={ this.props.logOut }
+                  onTouchTap={this.props.logOut}
                   primaryText="Log Out"
                 />
               </IconMenu>
@@ -110,6 +96,6 @@ export default class NavBar extends React.Component {
           false
         )}
       </div>
-    )
+    );
   }
 }
