527f00e4a3db3dfb3244aff790686dde2fac3d43
nisalVD <nisalvd@gmail.com>
2018-02-10 21:49:31 +1100

    fix default headers being set instead of temporary header for oauth

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,72 +1,95 @@
-import api from './init'
-import { setToken } from './init'
-import { getDecodedToken } from './token';
+import api from "./init"
+import { setToken } from "./init"
+import { getDecodedToken } from "./token"
 
 const appId = process.env.REACT_APP_CONCTR_APP_API_ID
 let handleErrors
 export function init(handleError) {
-    handleErrors = handleError
+  handleErrors = handleError
 }
 
 export function signIn({ email, password }) {
-    return api.post(`/consumers/admin/${appId}/login`, {
-	"userData": {
-		"email": email,
-		"pwd": password
-	}
-})
-  .then(res => res.data)
-  .catch(error => {
-    throw Error(error.response.data.error)})
+  return api
+    .post(`/consumers/admin/${appId}/login`, {
+      userData: {
+        email: email,
+        pwd: password
+      }
+    })
+    .then(res => res.data)
+    .catch(error => {
+      throw Error(error.response.data.error)
+    })
 }
 
 export function register({ email, password }) {
-    return api.post(`/consumers/admin/${appId}/register`, {
-	"userData": {
-		"email": email,
-		"pwd": password
-	}
-})
-  .then(res => res.data)
-  .catch(error => {
-    throw Error(error.response.data.error)})
+  return api
+    .post(`/consumers/admin/${appId}/register`, {
+      userData: {
+        email: email,
+        pwd: password
+      }
+    })
+    .then(res => res.data)
+    .catch(error => {
+      throw Error(error.response.data.error)
+    })
 }
 
-export function authSignIn(email,provider,access_token) {
-  api.defaults.headers['Authorization'] = `oth:${access_token}`
-  return api.post(`/consumers/admin/${appId}/oauth/login`, {
-  "userData": {
-    "email": email
-    },
-  "provider": provider
-  }
-  )
-  .then(res => {
-    const token = res.data.jwt
-    setToken(token)
-    return getDecodedToken()
-  })
-  .catch(error => {
-    throw Error(error.response.data.error)})
-  }
+export function authSignIn(email, provider, access_token) {
+  return api
+    .post(
+      `/consumers/admin/${appId}/oauth/login`,
+      {
+        userData: {
+          email: email
+        },
+        provider: provider
+      },
+      {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `oth:${access_token}`
+        }
+      }
+    )
+    .then(res => {
+      const token = res.data.jwt
+      setToken(token)
+      return getDecodedToken()
+    })
+    .catch(error => {
+      throw Error(error.response.data.error)
+    })
+}
 
-export function authRegister(email,provider,access_token) {
-  api.defaults.headers['Authorization'] = `oth:${access_token}`
-  return api.post(`/consumers/admin/${appId}/oauth/register`, {
-  "userData": {
-    "email": email
-    },
-  "provider": provider
-  }
-  )
-  .then(res => {
-    const token = res.data.jwt
-    setToken(token)
-    return getDecodedToken()
-  })
-  .catch(error => {
-    handleErrors(error.response.data.error)})
-  }
+export function authRegister(email, provider, access_token) {
+  api.defaults.headers["Authorization"] = `oth:${access_token}`
+  return api
+    .post(
+      `/consumers/admin/${appId}/oauth/register`,
+      {
+        userData: {
+          email: email
+        },
+        provider: provider
+      },
+      {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `oth:${access_token}`
+        }
+      }
+    )
+    .then(res => {
+      const token = res.data.jwt
+      setToken(token)
+      return getDecodedToken()
+    })
+    .catch(error => {
+      handleErrors(error.response.data.error)
+    })
+}
 
 export function signOutNow() {
   setToken(null)
